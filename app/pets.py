b91884bc1096948189fe384ab59d7678ea760540
Joel Lawi <abecedegluu@gmail.com>
2023-02-10 16:48:37 -0500

    tercer commit

--- app/pets.py
+++ app/pets.py
@@ -5,20 +5,37 @@ from app.models.pet import Pet
 from app.models.user import User
 from app.form import CreateForm, PetForm
 
-# from flask_login import current_user
-
 bp = Blueprint("pets_bp", __name__)
 
-@bp.route("/pets")
+@bp.route("/pets" , methods=['GET','POST'])
 def pets():
-    users = User.query.all()
-    pets = Pet.query.all()
-    return render_template("pets.html", pets= pets , users= users)
+    if request.method == 'POST':
+        search = request.form.get('search')
+        sort_by = request.form.get('sort_by')
+        if search:
+            pets = Pet.query.filter(Pet.name_pet.contains(search)).all()
+        else:
+            if sort_by == 'birthdate':
+                pets = Pet.query.order_by(Pet.birthdate).all()
+            elif sort_by == 'name_pet':
+                pets = Pet.query.order_by(Pet.name_pet).all()
+            elif sort_by == 'race':
+                pets = Pet.query.order_by(Pet.race).all()
+            elif sort_by == 'username':
+                pets = Pet.query.join(User).order_by(User.username).all()
+            else:
+                pets = Pet.query.all()
+    else:
+        pets = Pet.query.all()
 
-@bp.route('/pets/<id>')
-def pet_id(id):
-    pet = Pet.query.get(id)
-    return render_template("pet_user.html", pet=pet)
+    users = User.query.all()
+    return render_template("pets.html", pets=pets, users=users)
+
+@bp.route('/<username>')
+def pet_id(username):
+    user = User.query.filter_by(username=username).first()
+    pets = Pet.query.filter(user == Pet.user).all()
+    return render_template("pet_user.html", pets=pets)
 
 @bp.route("/pet/create", methods=['GET','POST'])
 def create_pet():
@@ -64,12 +81,3 @@ def update_pet(id):
     form.birthdate.data = pet.birthdate
     form.user_id.data = pet.user_id
     return render_template("pet.html" , form=form)
-
-# @bp.route('/post/delete/<id>', methods=['GET'])
-# def delete_post(id):
-#     pet = Pet.query.get(id)
-#     if pet.user_id == current_user.id:
-#         db.session.delete(pet)
-#         db.session.commit()
-#         flash("Se ha eliminado con exito!")
-#     return redirect(url_for('pets_bp.pets'))
